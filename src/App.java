256e60cdb0eabd381c9bbaf95433c4b18bf6f98c
Phreathia <89539784+Phreathia@users.noreply.github.com>
2024-09-09 14:37:08 +0000

    help please

--- src/App.java
+++ src/App.java
@@ -1,10 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        String sdfsdf = "sdfsdf";
+        // demonstrate the use of a function
+        print(str:"Hello World!");
+        print("The sum of 5 and 3 is " + add(a:5, b:3));
+    
+        // this is all a silly exercise to demonstrate the use of functions
+        // here's the easy way to print the sum of 5 and 3
+        System.out.println("The sum of 5 and 3 is " + (5+3));
+        print()
+    }
+    // function to print a string
+    public static void print(String str){
+        System.out.println(str);
+    }
+    public static void print(){
+        System.out.println(x:"Thank you for your time.");
+    }
 
-        System.out.println(sdfsdf.indexOf("sdf") != -1);
-        
-
+    // function to add two numbers
+    public static int add(int a, int b){
+        return a + b; // return the sum of a and b
     }
 }
